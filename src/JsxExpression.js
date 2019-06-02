e5a44a78b6060898bf73c7e376a220fcc4019dbb
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-02 15:11:52 +0530

    Embedded Expression in JSX

---
+++ src/JsxExpression.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Expression() {
+  const name = "jane";
+  const element = <h1>Hello, {name}</h1>;
+  return <div>{element}</div>;
+}
+
+export default Expression;
