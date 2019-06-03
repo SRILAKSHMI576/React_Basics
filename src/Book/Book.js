8892cb9f4c094279f9df8ece98c8b854e5a28b40
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-03 23:38:49 +0530

    Apply styles for Book

--- src/Book/Book.js
+++ src/Book/Book.js
@@ -1,12 +1,18 @@
 import React from "react";
+import "./style.css";
 
 function Book() {
   return (
-    <div>
-      Name: Robin sharma; <br />
-      Author: Robin;
+    <div className="book">
+      <div className="details">
+        Name: Robin sharma <br />
+        Author: Robin
+      </div>
       <br />
-      <img src="https://timedotcom.files.wordpress.com/2015/06/521811839-copy.jpg" />
+      <img
+        className="image"
+        src="https://timedotcom.files.wordpress.com/2015/06/521811839-copy.jpg"
+      />
     </div>
   );
 }
