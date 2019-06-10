1c2105f4f5a8fe0d6a2559940a7049d007805216
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-10 12:21:16 +0530

    create Page Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Library from "./Library/Library";
+import Page from "./PageLayout/PageLayout";
 
 function App() {
   return (
     <div>
-      <Library />
+      <Page />
     </div>
   );
 }
