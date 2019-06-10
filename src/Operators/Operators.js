2a774dcb3c36687007cb88ba51ab8cea993fb556
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-10 16:17:08 +0530

    Add numbers in Component

--- src/Operators/Operators.js
+++ src/Operators/Operators.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 function Operators() {
-  return <div>Operators</div>;
+  return (
+    <div className="Cal">
+      <table>
+        <td>7</td>
+        <td>8</td>
+        <td>9</td>
+      </table>
+      <table>
+        <td>4</td>
+        <td>5</td>
+        <td>6</td>
+      </table>
+      <table>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </table>
+      <table>
+        <td>0</td>
+        <td>.</td>
+        <td>,</td>
+      </table>
+    </div>
+  );
 }
 
 export default Operators;
