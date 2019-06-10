2930111177b5730ef58a0c83f7dbbbea7c32fbfb
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-10 15:43:09 +0530

    create Calculator component

---
+++ src/Calculator/Calculator.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Numbers from "../Numbers/Numbers";
+
+function Calculator() {
+  return (
+    <div>
+      Calculator
+      <Numbers />
+    </div>
+  );
+}
+
+export default Calculator;
