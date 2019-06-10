7450ddb7d6dc1e7ae18cda7cdaa91006b302d84c
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-10 12:12:04 +0530

    Create page layout

---
+++ src/PageLayout/PageLayout.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+function Page() {
+  return (
+    <div className="Header">
+      <div className="header1">Geeks For Geeks</div>
+      <div className="para">Computer Science and Engineering</div>
+      <div className="footer">Footer</div>
+      <div className="menu">
+        <a href="#home">HOME</a>
+        <a href="#news">NEWS</a>
+        <a href="notifications">NOTIFICATIONS</a>
+      </div>
+    </div>
+  );
+}
+
+export default Page;
