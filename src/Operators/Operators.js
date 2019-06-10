e71de73db2664c4ac9a9ad7c94a97e4945f4e97e
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-10 16:40:54 +0530

    Add operators

--- src/Operators/Operators.js
+++ src/Operators/Operators.js
@@ -1,27 +1,40 @@
 import React from "react";
+import "./style.css";
 
 function Operators() {
   return (
     <div className="Cal">
       <table>
-        <td>7</td>
-        <td>8</td>
-        <td>9</td>
-      </table>
-      <table>
-        <td>4</td>
-        <td>5</td>
-        <td>6</td>
-      </table>
-      <table>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-      </table>
-      <table>
-        <td>0</td>
-        <td>.</td>
-        <td>,</td>
+        <tr>
+          <td>AC</td>
+          <td>+/-</td>
+          <td>%</td>
+          <td className="op">/</td>
+        </tr>
+        <tr>
+          <td>7</td>
+          <td>8</td>
+          <td>9</td>
+          <td className="op">*</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td>
+          <td className="op">-</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td className="op">+</td>
+        </tr>
+        <tr>
+          <td>0</td>
+          <td> </td>
+          <td>,</td>
+          <td className="op">=</td>
+        </tr>
       </table>
     </div>
   );
