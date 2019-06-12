349a1a98f49e947bf1afb93e9868521d6e3e382c
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-12 17:03:19 +0530

    Craete title and dial at header

---
+++ src/Lubricants/Lubricants.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+import Headermenu from "../Headermenu/Headermenu";
+
+function Lubricants() {
+  return (
+    <div className="lubricants">
+      <b className="lubri">LUBRICANTS</b>
+      <b className="call">CALL TOLL FREE 1-800-772-5823</b>
+      <Headermenu />
+    </div>
+  );
+}
+
+export default Lubricants;
