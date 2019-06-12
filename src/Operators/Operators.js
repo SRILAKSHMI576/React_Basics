246900bee0e1d48164946b254d520e9c72cd3e85
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-12 14:15:10 +0530

    Create Operators

--- src/Operators/Operators.js
+++ src/Operators/Operators.js
@@ -3,39 +3,10 @@ import "./style.css";
 
 function Operators() {
   return (
-    <div className="Cal">
-      <table>
-        <tr>
-          <td>AC</td>
-          <td>+/-</td>
-          <td>%</td>
-          <td className="op">/</td>
-        </tr>
-        <tr>
-          <td>7</td>
-          <td>8</td>
-          <td>9</td>
-          <td className="op">*</td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>5</td>
-          <td>6</td>
-          <td className="op">-</td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>2</td>
-          <td>3</td>
-          <td className="op">+</td>
-        </tr>
-        <tr>
-          <td>0</td>
-          <td> </td>
-          <td>,</td>
-          <td className="op">=</td>
-        </tr>
-      </table>
+    <div className="operators">
+      <button className="operator">AC</button>
+      <button className="operator">+/-</button>
+      <button className="operator">%</button>
     </div>
   );
 }
