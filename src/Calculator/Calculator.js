3f4296919d718b11c6129b44c2ea90021c720809
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-12 14:17:28 +0530

    Create Calculator component

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Numbers from "../Numbers/Numbers";
+import Operators from "../Operators/Operators";
+import "./style.css";
+import ArthimaticOperators from "../Arthematic/Arthimatic";
 
 function Calculator() {
   return (
-    <div>
-      <Numbers />
+    <div className="calculator">
+      <div className="result">1</div>
+
+      <div className="container">
+        <div className="op-num-container">
+          <Operators />
+          <Numbers />
+        </div>
+        <ArthimaticOperators />
+      </div>
     </div>
   );
 }
