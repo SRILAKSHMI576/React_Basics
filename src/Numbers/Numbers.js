fc7870072881fd6c5ee17c72259d56fb30ab79f1
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-12 14:17:45 +0530

    Write Numbers

--- src/Numbers/Numbers.js
+++ src/Numbers/Numbers.js
@@ -1,10 +1,24 @@
 import React from "react";
-import Operators from "../Operators/Operators";
+import "./style.css";
 
 function Numbers() {
   return (
-    <div>
-      <Operators />
+    <div className="numbers">
+      <button className="number">7</button>
+      <button className="number">8</button>
+      <button className="number">9</button>
+
+      <button className="number">4</button>
+      <button className="number">5</button>
+      <button className="number">6</button>
+
+      <button className="number">1</button>
+      <button className="number">2</button>
+      <button className="number">3</button>
+
+      <button className="number">0</button>
+      <button className="number"> </button>
+      <button className="number">,</button>
     </div>
   );
 }
