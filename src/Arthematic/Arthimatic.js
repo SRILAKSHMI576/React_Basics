f27104305e183aa69d080e471c7a935b5b220a77
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-12 14:16:08 +0530

    Write Arthimatic Operators

---
+++ src/Arthematic/Arthimatic.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function ArthimaticOperators() {
+  return (
+    <div className="arthimatic-operators">
+      <button className="arthimatic-operator">/</button>
+      <button className="arthimatic-operator">*</button>
+      <button className="arthimatic-operator">-</button>
+      <button className="arthimatic-operator">+</button>
+      <button className="arthimatic-operator">=</button>
+    </div>
+  );
+}
+
+export default ArthimaticOperators;
