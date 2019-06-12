b0fd8a55e96b58cc036ad90f947c27d799f8c695
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-12 17:04:08 +0530

    Create menus in header

---
+++ src/Headermenu/Headermenu.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+function Headermenu() {
+  return (
+    <div className="menu">
+      <a href="#home">HOME</a>
+      <a href="#market-segments">MARKET SEGMENTS</a>
+      <a href="#services">SERVICES</a>
+      <a href="#products">PRODUCTS</a>
+      <a href="#promotions">PROMOTIONS</a>
+      <a href="#market-area">MARKET AREA</a>
+      <a href="market-information">MARKET INFORMATION</a>
+    </div>
+  );
+}
+
+export default Headermenu;
