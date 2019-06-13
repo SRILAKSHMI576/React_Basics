ba6e1743d2f4b888c7ec91a343d3955d1f3fc68a
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-13 18:26:16 +0530

    Create Title and toll free number in header

---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <div className="lubricants">
+      <b className="lubri">LUBRICANTS</b>
+      <b className="call">CALL TOLL FREE 1-800-772-5823</b>
+    </div>
+  );
+}
+
+export default Header;
