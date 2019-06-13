1fbc89ea48d47701e7ffed3e84be80e70a41308a
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-13 18:42:53 +0530

    Apply styles for Services

--- src/Headermenu/Headermenu.js
+++ src/Headermenu/Headermenu.js
@@ -3,14 +3,16 @@ import "./style.css";
 
 function Headermenu() {
   return (
-    <div className="menu">
-      <a href="#home">HOME</a>
-      <a href="#market-segments">MARKET SEGMENTS</a>
-      <a href="#services">SERVICES</a>
-      <a href="#products">PRODUCTS</a>
-      <a href="#promotions">PROMOTIONS</a>
-      <a href="#market-area">MARKET AREA</a>
-      <a href="market-information">MARKET INFORMATION</a>
+    <div className="header-menu">
+      <div className="menu">
+        <a href="#home">HOME</a>
+        <a href="#market-segments">MARKET SEGMENTS</a>
+        <a href="#services">SERVICES</a>
+        <a href="#products">PRODUCTS</a>
+        <a href="#promotions">PROMOTIONS</a>
+        <a href="#market-area">MARKET AREA</a>
+        <a href="market-information">MARKET INFORMATION</a>
+      </div>
     </div>
   );
 }
