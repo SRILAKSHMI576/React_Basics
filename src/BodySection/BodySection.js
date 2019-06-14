4878bcc72bba26442a1d4305a10b28fe4c388413
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-14 23:45:26 +0530

    Update styles in Bodysection

--- src/BodySection/BodySection.js
+++ src/BodySection/BodySection.js
@@ -10,25 +10,27 @@ function BodySection() {
       <div className="about-ppc">
         <div className="about">
           <h3 className="head">ABOUT PPC LUBRAICANTS</h3>
-          <h4>A Heritage of Specializing in Lubricants</h4>
-          <p>
-            For over 40 years, PPC Lubricants has earned a solid reputaion for
-            offering high quality products and serivices that improve your
-            business. How?By providing customres retention programs/products for
-            installers and products and serivices that increase equipment life
-            and enhance the efficiency of all equipment whether it be
-            <b>Automotive, Heavy Duty Commerical, or Industrial</b> in its
-            application.
-          </p>
-          <p>
-            At <b>PPC Lubricants,</b>We take tremendous pride in providing the
-            highest level of Service. When you call our toll free numbers you
-            will be greeted by a staff that is knowledgeable and experienced in
-            the various lubricants that we carry. Each custmores service
-            representative receives industry specific training on the latest
-            advancements in <b>Lubrication Technology</b>including changing
-            equipment and OEM requirements.
-          </p>
+          <div className="description">
+            <h4>A Heritage of Specializing in Lubricants</h4>
+            <p>
+              For over 40 years, PPC Lubricants has earned a solid reputaion for
+              offering high quality products and serivices that improve your
+              business. How?By providing customres retention programs/products
+              for installers and products and serivices that increase equipment
+              life and enhance the efficiency of all equipment whether it be
+              <b>Automotive, Heavy Duty Commerical, or Industrial</b> in its
+              application.
+            </p>
+            <p>
+              At <b>PPC Lubricants,</b>We take tremendous pride in providing the
+              highest level of Service. When you call our toll free numbers you
+              will be greeted by a staff that is knowledgeable and experienced
+              in the various lubricants that we carry. Each custmores service
+              representative receives industry specific training on the latest
+              advancements in <b>Lubrication Technology</b>including changing
+              equipment and OEM requirements.
+            </p>
+          </div>
         </div>
       </div>
     </div>
