53ce7864a24b4b0d75db63c4d90c5d1130cb1b0a
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-15 22:44:18 +0530

    Create Service component reuse it

---
+++ src/Service/Service.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Service() {
+  return (
+    <div className="name1">
+      <h5 className="item1">AUTOMOTIVE</h5>
+      <img
+        id="picture"
+        src="https://images.unsplash.com/photo-1504222490345-c075b6008014?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+      />
+      <p className="paragraph">
+        The Automotive Maarket requires a diffreent set of tools in order to
+        succeed. PPC Lubricants offers various programs that are customized to
+        each custmores in..<button>READ MORE</button>
+      </p>
+    </div>
+  );
+}
+
+export default Service;
--- src/Services/Services.js
+++ src/Services/Services.js
@@ -1,57 +1,14 @@
 import React from "react";
 import "./styles.css";
+import Service from "../Service/Service";
 
 function Services() {
   return (
     <div className="services">
-      <div className="name1">
-        <h5 className="item1">AUTOMOTIVE</h5>
-        <img
-          id="picture"
-          src="https://images.unsplash.com/photo-1504222490345-c075b6008014?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-        />
-        <p className="paragraph">
-          The Automotive Maarket requires a diffreent set of tools in order to
-          succeed. PPC Lubricants offers various programs that are customized to
-          each custmores in..<button>READ MORE</button>
-        </p>
-      </div>
-      <div className="name2">
-        <h5 className="item2">AUTOMOTIVE</h5>
-        <img
-          id="picture"
-          src="https://images.unsplash.com/photo-1504222490345-c075b6008014?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-        />
-        <p className="paragraph">
-          The Automotive Maarket requires a diffreent set of tools in order to
-          succeed. PPC Lubricants offers various programs that are customized to
-          each custmores in..<button>READ MORE</button>
-        </p>
-      </div>
-      <div className="name3">
-        <h5 className="item3">AUTOMOTIVE</h5>
-        <img
-          id="picture"
-          src="https://images.unsplash.com/photo-1504222490345-c075b6008014?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-        />
-        <p className="paragraph">
-          The Automotive Maarket requires a diffreent set of tools in order to
-          succeed. PPC Lubricants offers various programs that are customized to
-          each custmores in.. <button>READ MORE</button>
-        </p>
-      </div>
-      <div className="name4">
-        <h5 className="item4">AUTOMOTIVE</h5>
-        <img
-          id="picture"
-          src="https://images.unsplash.com/photo-1504222490345-c075b6008014?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-        />
-        <p className="paragraph">
-          The Automotive Maarket requires a diffreent set of tools in order to
-          succeed. PPC Lubricants offers various programs that are customized to
-          each custmores in..<button>READ MORE</button>
-        </p>
-      </div>
+      <Service />
+      <Service />
+      <Service />
+      <Service />
     </div>
   );
 }
