70d06ecd1bd2294c3560a77c5e1062fab1bf2d52
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-16 12:32:28 +0530

    Create index component in GridLayoutExample

---
+++ src/GridExmpale1/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./style.css";
+
+function Index() {
+  return (
+    <div className="wrapper">
+      <div>
+        A paragraph (from the Ancient Greek παράγραφος paragraphos, "to write
+        beside" or "written beside") is a self-contained unit of a discourse in
+        writing dealing with a particular point or idea. A paragraph consists of
+        one or more sentences
+      </div>
+      <div>
+        A paragraph (from the Ancient Greek παράγραφος paragraphos, "to write
+        beside" or "written beside") is a self-contained unit of a discourse in
+        writing dealing with a particular point or idea. A paragraph consists of
+        one or more sentences
+      </div>
+      <div>
+        A paragraph (from the Ancient Greek παράγραφος paragraphos, "to write
+        beside" or "written beside") is a self-contained unit of a discourse in
+        writing dealing with a particular point or idea. A paragraph consists of
+        one or more sentences
+      </div>
+      <div>
+        A paragraph (from the Ancient Greek παράγραφος paragraphos, "to write
+        beside" or "written beside") is a self-contained unit of a discourse in
+        writing dealing with a particular point or idea. A paragraph consists of
+        one or more sentences
+      </div>
+    </div>
+  );
+}
+
+export default Index;
