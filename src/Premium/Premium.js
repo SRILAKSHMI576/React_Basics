f49273189cc806d4e675998fb0d505a0f3e43434
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-17 15:44:33 +0530

    Import Advertisement2 component in Premium

--- src/Premium/Premium.js
+++ src/Premium/Premium.js
@@ -2,12 +2,14 @@ import React from "react";
 import Advertisement from "../Advertisement/Advertisement";
 import "./style.css";
 import Advertisement1 from "../Advertisement1/Advertisement1";
+import Advertisement2 from "../Advertisement2/Advertisement2";
 
 function Premium() {
   return (
     <div className="premium">
       <Advertisement />
       <Advertisement1 />
+      <Advertisement2 />
     </div>
   );
 }
