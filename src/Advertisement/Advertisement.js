4e238a9132ba5a3e56cbb4f58cd3ada88d4788a1
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-17 15:35:50 +0530

    Update styles in Advertisement

--- src/Advertisement/Advertisement.js
+++ src/Advertisement/Advertisement.js
@@ -1,6 +1,61 @@
 import React from "react";
+import "./style.css";
 
 function Advertisement() {
-  return <di>Advertisement</di>;
+  return (
+    <div className="advertisement">
+      <div className="para">
+        <p className="para1">Hi, srinivasarao</p>
+        <p className="para2"> Custmoers since 2016</p>
+      </div>
+      <div className="para3">Top links for you</div>
+      <div className="images1">
+        <div>
+          <a href="https://www.amazon.in/">
+            <img
+              className="image2"
+              src="
+              amazon-boxes.jpg"
+            />
+            <p className="orders">Your Orders</p>
+          </a>
+        </div>
+        <div>
+          <a href="https://www.amazon.in/">
+            <img
+              className="image3"
+              src="
+              https://images-na.ssl-images-amazon.com/images/I/510d4L0LYKL._SY550_.jpg"
+            />
+            <p className="mobile">
+              Mobile and <br /> Accessories
+            </p>
+          </a>
+        </div>
+      </div>
+      <div className="images2">
+        <div>
+          <a href="https://www.amazon.in/">
+            <img
+              className="image1"
+              src="
+              https://images-na.ssl-images-amazon.com/images/I/81Jne9w3OML._UX466_.jpg"
+            />
+            <p className="Fashion">Fashion</p>
+          </a>
+        </div>
+        <div>
+          <a href="https://www.amazon.in/">
+            <img
+              className="image4"
+              src="
+              https://images-na.ssl-images-amazon.com/images/I/811SAfnegNL._SY606_.jpg"
+            />
+            <p className="para4">Grocery</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Advertisement;
