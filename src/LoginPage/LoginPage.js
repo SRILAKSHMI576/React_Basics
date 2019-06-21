405bcea6788efb964c27f7fd331b1445be8fb25f
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-21 18:25:45 +0530

    Add header and Image in LoginPage

---
+++ src/LoginPage/LoginPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+function LoginPage() {
+  return (
+    <div>
+      <div className="head">
+        Facebook helps you connect and share with the people in your life.
+      </div>
+      <img
+        className="img"
+        src="http://www.mrsdingman.com/facebook_map.gif"
+        width="200%"
+        height="150%"
+      />
+    </div>
+  );
+}
+export default LoginPage;
