a42672bd3ceabda8f95fdb4387d4c05226886236
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-25 20:13:13 +0530

    Create Title component

---
+++ src/Ttitle/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+function Title() {
+  return (
+    <div className="title">
+      <p>Company name</p>
+    </div>
+  );
+}
+export default Title;
