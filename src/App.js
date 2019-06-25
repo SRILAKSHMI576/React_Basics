fddf26440cabdae370a2c30e5edc6d73dc7cda60
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-25 13:22:15 +0530

    Create texxtfield

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from "react";
-import FacebookHeader from "./FacebookHeader/FacebookHeader";
-import FbAccount from "./FbAccount/FbAccount";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import "./style.css";
 
 function App() {
   return (
-    <div>
-      <FacebookHeader />
-      <FbAccount />
+    <div className="app">
+      <TextField
+        className="field"
+        id="outlined-name"
+        label="Email Address"
+        margin="normal"
+        variant="outlined"
+      />
+      <br />
+
+      <TextField
+        className="field"
+        id="outlined-name"
+        label="Password"
+        margin="normal"
+        variant="outlined"
+      />
+      <br />
+
+      <Button className="sign-up" variant="contained" color="primary">
+        Sign Up
+      </Button>
     </div>
   );
 }
