7189169fe224bb7e7062564c21ba02e62f460406
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-26 20:18:13 +0530

    Create AllProjects Component

---
+++ src/AllProjects/AllProjects.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Calculator from "../Calculator/Calculator";
+import Facebook from "../Facebook/Facebook";
+import Amazon from "../Amazon/Amazon";
+import Lubricants from "../Lubricants/Lubricants";
+import Library from "../Library/Library";
+import Index1 from "../GridExmaple2/index1";
+import Index from "../GridExmpale1";
+import GridLayout from "../GridLayoutExamples/GridLayout";
+import Time from "../Time";
+
+function AllProjects() {
+  return (
+    <div>
+      <Router>
+        <div>
+          <Route path="/calculator" component={Calculator} />
+          <Route path="/facebook" component={Facebook} />
+          <Route path="/amazon-premium" component={Amazon} />
+          <Route path="/lubricants" component={Lubricants} />
+          <Route path="/library" component={Library} />
+          <Route path="/index-1" component={Index1} />
+          <Route path="/index" component={Index} />
+          <Route path="/grid-layout" component={GridLayout} />
+          <Route path="/time" component={Time} />
+        </div>
+      </Router>
+    </div>
+  );
+}
+export default AllProjects;
