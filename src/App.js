6829cceb592c6e0baf6a209172bc21b48e8e2cd7
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-26 21:56:50 +0530

    import AllProjects component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React from "react";
 import Checkout from "./Checkout/Checkout";
-import Title from "./Ttitle/Title";
 
 function App() {
   return (
     <div className="app">
-      {/* <TextField
-        className="field"
-        id="outlined-name"
-        label="Email Address"
-        margin="normal"
-        variant="outlined"
-      />
-      <br /> */}
-
-      {/* <TextField
-        className="field"
-        id="outlined-name"
-        label="Password"
-        margin="normal"
-        variant="outlined"
-      />
-      <br />
-
-      <Button className="sign-up" variant="contained" color="primary">
-        Sign Up
-      </Button> */}
-      <Title />
-      <Checkout />
+      <div>
+        <Checkout />
+      </div>
     </div>
   );
 }
