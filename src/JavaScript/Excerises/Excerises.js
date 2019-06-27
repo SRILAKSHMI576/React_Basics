361d5d6923f9f5d19c514160735fd61522a3d8b9
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-27 12:42:11 +0530

    Create Excerises Component

---
+++ src/JavaScript/Excerises/Excerises.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Excerises() {
+  var m = 5;
+  var square = m * m;
+  var cube = m * m * m;
+  return (
+    <div>
+      {m} Square = {square}
+    </div>
+  );
+}
+export default Excerises;
