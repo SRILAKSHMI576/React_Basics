8f3c6bddcd86347d1ef51f4ea6676293f872b0e8
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-27 12:49:27 +0530

    Print Sum of n natural numbers

--- src/JavaScript/Excerises/Excerises.js
+++ src/JavaScript/Excerises/Excerises.js
@@ -1,14 +1,18 @@
 import React from "react";
 
 function Excerises() {
-  var m = 5;
-  var square = m * m;
-  var cube = m * m * m;
+  var n = 5;
+  var square = n * n;
+  var cube = n * n * n;
+  var sum_of_natural = (n * (n + 1)) / 2;
+
   return (
     <div>
       {m} Square = {square}
       <br />
       {m} Cube = {cube}
+      <br />
+      {m} Sum Of natural numbers = {sum_of_natural}
     </div>
   );
 }
