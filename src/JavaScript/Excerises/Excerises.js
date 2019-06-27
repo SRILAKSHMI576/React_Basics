3adfb66aa2300d6ecd07251c89967c8e7af55382
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-27 13:08:17 +0530

    Print Rectangle Perimeter

--- src/JavaScript/Excerises/Excerises.js
+++ src/JavaScript/Excerises/Excerises.js
@@ -6,6 +6,10 @@ function Excerises() {
   var cube = n * n * n;
   var sum_of_natural_number = (n * (n + 1)) / 2;
   var sum_of_natural_squares = (n * (n + 2) * (2 * n + 1)) / 6;
+  var length = 3;
+  var bredth = 4;
+  var reactangle_area = length * bredth;
+  var reactangle_perimeter = 2 * (length + bredth);
 
   return (
     <div>
@@ -13,9 +17,13 @@ function Excerises() {
       <br />
       {n} Cube = {cube}
       <br />
-      Sum Of natural n numbers = {sum_of_natural_number}
+      Sum Of natural {n} numbers = {sum_of_natural_number}
       <br />
-      Sum of natural n number squares = {sum_of_natural_squares}
+      Sum of natural {n} number squares = {sum_of_natural_squares}
+      <br />
+      Rectangle Area = {reactangle_area}
+      <br />
+      Rectangle Perimeter = {reactangle_perimeter}
     </div>
   );
 }
