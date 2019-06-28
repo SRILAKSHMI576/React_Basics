c194871e6251a555a324e0cc4c97d9bde27f2b75
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-29 00:44:21 +0530

    Create UdemyHomePage

---
+++ src/UdemyHomePage/UdemyHomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import UdemyHeader from "../UdemyHeader/UdemyHeader";
+
+function UdemyHomePage() {
+  return (
+    <div>
+      <UdemyHeader />
+    </div>
+  );
+}
+export default UdemyHomePage;
