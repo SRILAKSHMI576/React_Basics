698cf0c35944f24865231e0b79900fda9106a696
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-28 20:15:40 +0530

    Move Servie to Lubricants

---
+++ src/Lubricants/Service/Service.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Service(props) {
+  console.log(props);
+  return (
+    <div className="name1">
+      <h5 className="item1">{props.title}</h5>
+      <img
+        id="picture"
+        src="https://images.unsplash.com/photo-1504222490345-c075b6008014?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+      />
+      <p className="paragraph">
+        {props.description}
+        <button>READ MORE</button>
+      </p>
+    </div>
+  );
+}
+
+export default Service;
