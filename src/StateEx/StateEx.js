67ce07583c07ed1426048094f4130809ba49ec8b
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-29 13:07:14 +0530

    Create StateEx component

---
+++ src/StateEx/StateEx.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+class StateEx extends React.Component {
+  state = {
+    a: 10,
+    b: 5
+  };
+  setA = e => {
+    this.setState({
+      a: parseInt(e.target.value)
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <input onChange={this.setA} />
+        <input />
+        <br />
+        {"A:" + this.state.a}
+        {" B: " + this.state.b}
+        <br />
+        {"Square: " + this.state.a ** 2}
+        {" Cube:" + this.state.b}
+      </div>
+    );
+  }
+}
+
+export default StateEx;
