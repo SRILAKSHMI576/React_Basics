2a4830b36da3acb6a2e9fff5920b3745f1e7efb5
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-30 07:00:15 +0530

    Apply styles for UdemyHomePage

--- src/UdemyHomePage/UdemyHomePage.js
+++ src/UdemyHomePage/UdemyHomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import UdemyHeader from "../UdemyHeader/UdemyHeader";
+import UdemyMenu from "../UdemyMenu/UdemyMenu";
 
 function UdemyHomePage() {
   return (
     <div>
       <UdemyHeader />
+      <UdemyMenu />
     </div>
   );
 }
