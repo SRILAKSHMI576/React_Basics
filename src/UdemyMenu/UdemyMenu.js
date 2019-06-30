56dc3b12f00d73a667985823bac5101f4c08b193
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-06-30 06:59:36 +0530

    Craete UdemyMenu component

---
+++ src/UdemyMenu/UdemyMenu.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+
+function UdemyMenu() {
+  return (
+    <div className="header-menu">
+      <div className="head-menu">
+        <a href="https://www.udemy.com/">Development</a>
+        <a href="https://www.udemy.com/">Business</a>
+        <a href="https://www.udemy.com/">IT & Software</a>
+        <a href="https://www.udemy.com/">Office Productvity</a>
+        <a href="https://www.udemy.com/">Personal Development</a>
+        <a href="https://www.udemy.com/">Design</a>
+        <a href="https://www.udemy.com/">Marketing</a>
+        <a href="https://www.udemy.com/">Health & Fitness</a>
+        <a href="https://www.udemy.com/">Music</a>
+      </div>
+    </div>
+  );
+}
+export default UdemyMenu;
