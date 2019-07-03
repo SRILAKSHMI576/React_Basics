94751c3de0dae7fc94607630d997c63a8492e86a
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-03 13:54:40 +0530

    Create advertisements and apply styles for UdemyAdds

--- src/UdemyHomePage/UdemyHomePage.js
+++ src/UdemyHomePage/UdemyHomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import UdemyHeader from "../UdemyHeader/UdemyHeader";
 import UdemyMenu from "../UdemyMenu/UdemyMenu";
+import UdemyAdds from "../UdemyAdds/UdemyAdds";
 
 function UdemyHomePage() {
   return (
     <div>
       <UdemyHeader />
       <UdemyMenu />
+      <UdemyAdds />
     </div>
   );
 }
