bd3bb7950fa65d87f49abc4ec6c6a7985917367f
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-09 17:59:13 +0530

    Create GeeksCalButton component and create button in component

---
+++ src/GeeksCalButtons/GeeksCalButtons.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function GeeksCalButtons() {
+  return (
+    <div>
+      <div className="button-row">
+        <button>Clear</button>
+        <button>Delete</button>
+        <button>.</button>
+        <button>/</button>
+      </div>
+    </div>
+  );
+}
+
+export default GeeksCalButtons;
