1665fc496dc723f49223a79c562abadc97466fb1
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-11 10:34:28 +0530

    Create Login Logout buttons and functionalities

--- src/LogoutLogin/LogoutLogin.js
+++ src/LogoutLogin/LogoutLogin.js
@@ -1,9 +1,43 @@
 import React from "react";
+import { func } from "prop-types";
 
+//Message Component
+function Message(props) {
+  if (props.isLoggedIn) return <h1>Welcome User</h1>;
+  else return <h1>Please Login</h1>;
+}
+//Login Component
+function Login(props) {
+  return <button onClick={props.clickFunc}>Login</button>;
+}
+//Logout Component
+function Logout(props) {
+  return <button onClick={props.clickFunc}>Logout</button>;
+}
 class LogoutLogin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoggedIn: false };
+    this.ifLoginClicked = this.ifLoginClicked.bind(this);
+    this.ifLogoutClicked = this.ifLogoutClicked.bind(this);
+  }
+  ifLoginClicked() {
+    this.setState({ isLoggedIn: true });
+  }
+  ifLogoutClicked() {
+    this.setState({ isLoggedIn: false });
+  }
   render() {
-    return <div>hi</div>;
+    return (
+      <div>
+        <Message isLoggedIn={this.state.isLoggedIn} />
+        {this.state.isLoggedIn ? (
+          <Logout clickFunc={this.ifLogoutClicked} />
+        ) : (
+          <Login clickFunc={this.ifLoginClicked} />
+        )}
+      </div>
+    );
   }
 }
-
 export default LogoutLogin;
