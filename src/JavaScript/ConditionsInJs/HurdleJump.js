75d28589da51959ccab4689f834ff648f8524754
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-16 21:26:55 +0530

    Change values in state and create hurdleJump function

--- src/JavaScript/ConditionsInJs/HurdleJump.js
+++ src/JavaScript/ConditionsInJs/HurdleJump.js
@@ -1,8 +1,41 @@
 import React from "react";
+import { thisExpression } from "@babel/types";
 
 class HurdleJump extends React.Component {
+  state = {
+    hurdles: [1, 2, 3],
+    jumpHeight: 5
+  };
+  changeA = e => {
+    this.setState({
+      hurdles: e.target.value
+    });
+  };
+  changeB = e => {
+    this.setState({
+      jumpHeight: e.target.value
+    });
+  };
+
+  hurdleJump = () => {
+    var high = Math.max(...this.state.hurdles);
+    if (high > this.state.jumpHeight) {
+      return "false";
+    } else {
+      return "true";
+    }
+  };
   render() {
-    return <div>hi</div>;
+    return (
+      <div>
+        <input onChange={this.changeA} />
+        hurdles: {this.state.hurdles}
+        <input onChange={this.changeB} />
+        jumpHeight: {this.state.jumpHeight}
+        <br />
+        {this.hurdleJump()}
+      </div>
+    );
   }
 }
 
