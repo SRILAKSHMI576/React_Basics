2e9c528d305231e4f7856690ccabc4ec680aa1f9
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-16 19:25:58 +0530

    Change values in state and create a evenOrOdd function

--- src/JavaScript/ConditionsInJs/EvenOdd.js
+++ src/JavaScript/ConditionsInJs/EvenOdd.js
@@ -1,10 +1,28 @@
 import React from "react";
 
 class EvenOdd extends React.Component {
+  state = {
+    a: 5
+  };
+  changeA = e => {
+    this.setState({
+      a: e.target.value
+    });
+  };
+
+  evenOrOdd = () => {
+    if (this.state.a % 2 == 0) {
+      return "even";
+    } else {
+      return "odd";
+    }
+  };
   render() {
     return (
       <div>
-        <input />
+        <input onChange={this.changeA} />
+        {this.state.a}
+        {this.evenOrOdd()}
       </div>
     );
   }
