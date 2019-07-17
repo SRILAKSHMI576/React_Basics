561eb390d2aadfe8230c4c0480dde6171700acb1
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-17 14:13:52 +0530

    Create setState and change values in input

--- src/JavaScript/ConditionsInJs/Factorial.js
+++ src/JavaScript/ConditionsInJs/Factorial.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 class Factorial extends React.Component {
+  state = {
+    num: 9
+  };
+  chnageNum = e => {
+    this.setState({
+      num: e.target.value
+    });
+  };
   render() {
     return (
       <div>
-        <input />
+        <input onChange={this.chnageNum} />
+        {this.state.num}
       </div>
     );
   }
