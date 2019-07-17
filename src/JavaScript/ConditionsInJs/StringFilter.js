06f487840fd70f80693036530e6a3be2923d18f6
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-17 10:29:11 +0530

    Create StringFilter component and create function to find string

---
+++ src/JavaScript/ConditionsInJs/StringFilter.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class StringFilter extends React.Component {
+  state = {
+    arr: [[1, 2, 4, "a", "@"]]
+  };
+  changeArr = e => {
+    this.setState({
+      arr: e.target.value
+    });
+  };
+
+  outStringsFromList = () => {
+    return this.arr.filter(arr => typeof arr === "number");
+  };
+  render() {
+    return (
+      <div>
+        <input onChange={this.changeArr} />
+        a: {this.state.arr} <br />
+        number List: {this.outStringsFromList()}
+      </div>
+    );
+  }
+}
+
+export default StringFilter;
