0b364a303e75d58e034f854c77eb6384ee6e79eb
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-17 13:49:39 +0530

    chnage state value and display find letter string in sentence

--- src/JavaScript/ConditionsInJs/FourLetterStrings.js
+++ src/JavaScript/ConditionsInJs/FourLetterStrings.js
@@ -1,10 +1,23 @@
 import React from "react";
 
 class FourLetterStrings extends React.Component {
+  state = {
+    str: ["Ryan ", "Kieran ", "Jason ", "Matt"]
+  };
+  changeString = e => {
+    this.setState({
+      str: e.target.value
+    });
+  };
+  fourLetterString = () => {
+    return this.str.filter(word => word.length == 4);
+  };
   render() {
     return (
       <div>
-        <input />
+        <input onChange={this.changeString} />
+        {this.state.str} <br />
+        {this.fourLetterString()}
       </div>
     );
   }
