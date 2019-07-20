70a975cc2bbfa41f15fc9bc653ed880ba5a8f4b5
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-20 16:14:19 +0530

    Create UserOutput compoenent and import in UdemyAssignment

--- src/UdemyAssignment/UdemyAssignment.js
+++ src/UdemyAssignment/UdemyAssignment.js
@@ -1,10 +1,12 @@
 import React from "react";
 import UserInput from "../UserInput/UserInput";
+import UserOutput from "../UserOutput/UserOutput";
 
 function UdemyAssignment() {
   return (
     <div>
       <UserInput />
+      <UserOutput />
     </div>
   );
 }
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserOutput() {
+  return <div>hello</div>;
+}
+
+export default UserOutput;
