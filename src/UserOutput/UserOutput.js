54fe72269afc5ad8973ba6a629ab2bf90a5110bc
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-20 16:17:01 +0530

    Write two paragraphs in UserOutput

--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function UserOutput() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <p>
+        This paragraph contains a lot of lines in the source code, but the
+        browser ignores it.
+      </p>
+
+      <p>
+        This paragraph contains a lot of spaces in the source code, but the
+        browser ignores it.
+      </p>
+    </div>
+  );
 }
 
 export default UserOutput;
