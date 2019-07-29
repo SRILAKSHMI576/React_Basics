cd574ad96cbfc4d437be7711180dbeb1530a38d7
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-29 23:30:36 +0530

    Create href links

--- src/Hacker Header/HackerHeader.js
+++ src/Hacker Header/HackerHeader.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function HackerHeader() {
-  return <div>hello</div>;
+  return (
+    <div className="hacker-header">
+      <a href="#">Hacker News</a>
+      <a href="#">new</a> |<a href="#">past</a> |<a href="#">comments</a> |
+      <a href="#">ask</a> |<a href="#">show</a> |<a href="#">jobs</a> |
+      <a href="#">submit</a>
+    </div>
+  );
 }
 
 export default HackerHeader;
