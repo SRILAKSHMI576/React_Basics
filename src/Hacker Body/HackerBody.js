d38a13fe145bc512b4dcf2878c591dd86e7827eb
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-30 02:34:07 +0530

    Apply styles for HackerHeader

--- src/Hacker Body/HackerBody.js
+++ src/Hacker Body/HackerBody.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 function HackerBody() {
-  return <div>hiiiiii</div>;
+  return (
+    <div>
+      <p>
+        1. Why Lie Detector Tests Can’t Be Trusted{" "}
+        <a href="#" className="smith">
+          (smithsonianmag.com)
+        </a>{" "}
+      </p>
+    </div>
+  );
 }
+
 export default HackerBody;
--- src/Hacker Header/HackerHeader.js
+++ src/Hacker Header/HackerHeader.js
@@ -3,13 +3,20 @@ import "./style.css";
 
 function HackerHeader() {
   return (
-    <div className="hacker-header">
-      <a href="#">Hacker News</a>
-      <a href="#">new</a> |<a href="#">past</a> |<a href="#">comments</a> |
-      <a href="#">ask</a> |<a href="#">show</a> |<a href="#">jobs</a> |
-      <a href="#">submit</a>
-      <div className="log-in">
-        <a href="#">login</a>
+    <div className="hacker">
+      <div className="hacker-header">
+        <a href="#" className="y">
+          Y
+        </a>
+        <a href="#" className="hacker-1">
+          Hacker News
+        </a>
+        <a href="#">new</a> |<a href="#">past</a> |<a href="#">comments</a> |
+        <a href="#">ask</a> |<a href="#">show</a> |<a href="#">jobs</a> |
+        <a href="#">submit</a>
+        <div className="log-in">
+          <a href="#">login</a>
+        </div>
       </div>
     </div>
   );
