fdeca1ebb1d9d7f85261d716d0f548633bd6af66
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-30 03:20:19 +0530

    Apply styles for HackerBody

--- src/Hacker Body/HackerBody.js
+++ src/Hacker Body/HackerBody.js
@@ -1,13 +1,100 @@
 import React from "react";
+import "./style.css";
 
 function HackerBody() {
   return (
-    <div>
-      <p>
-        1. Why Lie Detector Tests Can’t Be Trusted{" "}
-        <a href="#" className="smith">
+    <div className="hacker-body">
+      <p className="news-paragraph">
+        1. Why Lie Detector Tests Can’t Be Trusted
+        <a href="#" className="hacker-links">
           (smithsonianmag.com)
-        </a>{" "}
+        </a>
+        <p className="news-para">
+          164 points by
+          <a href="#" className="hacker-links">
+            moltensodium
+          </a>
+          <a href="#" className="hacker-links">
+            1 hour ago
+          </a>
+          |
+          <a href="#" className="hacker-links">
+            hide
+          </a>
+          |
+          <a href="#" className="hacker-links">
+            82 comments
+          </a>
+        </p>
+      </p>
+      <p className="news-paragraph">
+        2. India tiger census shows rapid population growth
+        <a href="#" className="hacker-links">
+          (bbc.com)
+        </a>
+        <p className="news-para">
+          180 points by
+          <a href="#" className="hacker-links">
+            hhs
+          </a>
+          <a href="#" className="hacker-links">
+            5 hours ago
+          </a>
+          |
+          <a href="#" className="hacker-links">
+            hide
+          </a>
+          |
+          <a href="#" className="hacker-links">
+            64 comments
+          </a>
+        </p>
+      </p>
+      <p className="news-paragraph">
+        3. IUber Lays Off 400 as Profitability Doubts Linger After IPO
+        <a href="#" className="hacker-links">
+          (nytimes.com)
+        </a>
+        <p className="news-para">
+          164 points by
+          <a href="#" className="hacker-links">
+            hhs
+          </a>
+          <a href="#" className="hacker-links">
+            1 hour ago
+          </a>
+          |
+          <a href="#" className="hacker-links">
+            hide
+          </a>
+          |
+          <a href="#" className="hacker-links">
+            82 comments
+          </a>
+        </p>
+      </p>
+      <p className="news-paragraph">
+        4. Browser extensions are underrated: the promise of hackable software
+        <a href="#" className="hacker-links">
+          (geoffreylitt.com)
+        </a>
+        <p className="news-para">
+          179 points by
+          <a href="#" className="hacker-links">
+            pseudolus
+          </a>
+          <a href="#" className="hacker-links">
+            4 hours ago
+          </a>
+          |
+          <a href="#" className="hacker-links">
+            hide
+          </a>
+          |
+          <a href="#" className="hacker-links">
+            82 comments
+          </a>
+        </p>
       </p>
     </div>
   );
