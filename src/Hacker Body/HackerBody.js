11f82edd87696161cc625172c8b1a03b41e28102
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-30 04:01:59 +0530

    Create horizontal line and apply styles to line

--- src/Hacker Body/HackerBody.js
+++ src/Hacker Body/HackerBody.js
@@ -27,75 +27,316 @@ function HackerBody() {
           </a>
         </p>
       </p>
-      <p className="news-paragraph">
-        2. India tiger census shows rapid population growth
-        <a href="#" className="hacker-links">
-          (bbc.com)
-        </a>
-        <p className="news-para">
-          180 points by
+      <div className="spacing">
+        <p className="news-paragraph">
+          2. India tiger census shows rapid population growth
           <a href="#" className="hacker-links">
-            hhs
+            (bbc.com)
           </a>
-          <a href="#" className="hacker-links">
-            5 hours ago
-          </a>
-          |
-          <a href="#" className="hacker-links">
-            hide
-          </a>
-          |
-          <a href="#" className="hacker-links">
-            64 comments
-          </a>
+          <p className="news-para">
+            180 points by
+            <a href="#" className="hacker-links">
+              hhs
+            </a>
+            <a href="#" className="hacker-links">
+              5 hours ago
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              hide
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              64 comments
+            </a>
+          </p>
         </p>
-      </p>
-      <p className="news-paragraph">
-        3. IUber Lays Off 400 as Profitability Doubts Linger After IPO
-        <a href="#" className="hacker-links">
-          (nytimes.com)
-        </a>
-        <p className="news-para">
-          164 points by
+      </div>
+      <div className="spacing">
+        <p className="news-paragraph">
+          3. IUber Lays Off 400 as Profitability Doubts Linger After IPO
           <a href="#" className="hacker-links">
-            hhs
+            (nytimes.com)
           </a>
-          <a href="#" className="hacker-links">
-            1 hour ago
-          </a>
-          |
-          <a href="#" className="hacker-links">
-            hide
-          </a>
-          |
-          <a href="#" className="hacker-links">
-            82 comments
-          </a>
+          <p className="news-para">
+            164 points by
+            <a href="#" className="hacker-links">
+              hhs
+            </a>
+            <a href="#" className="hacker-links">
+              1 hour ago
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              hide
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              82 comments
+            </a>
+          </p>
         </p>
-      </p>
-      <p className="news-paragraph">
-        4. Browser extensions are underrated: the promise of hackable software
-        <a href="#" className="hacker-links">
-          (geoffreylitt.com)
-        </a>
-        <p className="news-para">
-          179 points by
+      </div>
+      <div className="spacing">
+        <p className="news-paragraph">
+          4. Browser extensions are underrated: the promise of hackable software
           <a href="#" className="hacker-links">
-            pseudolus
+            (geoffreylitt.com)
           </a>
-          <a href="#" className="hacker-links">
-            4 hours ago
-          </a>
-          |
-          <a href="#" className="hacker-links">
-            hide
-          </a>
-          |
-          <a href="#" className="hacker-links">
-            82 comments
-          </a>
+          <p className="news-para">
+            179 points by
+            <a href="#" className="hacker-links">
+              pseudolus
+            </a>
+            <a href="#" className="hacker-links">
+              4 hours ago
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              hide
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              82 comments
+            </a>
+          </p>
         </p>
-      </p>
+      </div>
+      <div className="spacing">
+        <p className="news-paragraph">
+          5. Lambda School fined $75k by CA for operating without state approval
+          [pdf]
+          <a href="#" className="hacker-links">
+            (cs.gov)
+          </a>
+          <p className="news-para">
+            237 points by
+            <a href="#" className="hacker-links">
+              lanrh1836
+            </a>
+            <a href="#" className="hacker-links">
+              6 hours ago
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              hide
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              12 comments
+            </a>
+          </p>
+        </p>
+      </div>
+      <div className="spacing">
+        <p className="news-paragraph">
+          6. YouTube faces creator backlash
+          <a href="#" className="hacker-links">
+            (axios.com)
+          </a>
+          <p className="news-para">
+            244 points by
+            <a href="#" className="hacker-links">
+              hhs
+            </a>
+            <a href="#" className="hacker-links">
+              6 hours ago
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              hide
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              275 comments
+            </a>
+          </p>
+        </p>
+      </div>
+      <div className="spacing">
+        <p className="news-paragraph">
+          7. The Washington Post's bestseller lists have been wrong
+          <a href="#" className="hacker-links">
+            (washingtonpost.com)
+          </a>
+          <p className="news-para">
+            66 points by
+            <a href="#" className="hacker-links">
+              guuseom
+            </a>
+            <a href="#" className="hacker-links">
+              4 hours ago
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              hide
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              27 comments
+            </a>
+          </p>
+        </p>
+      </div>
+      <div className="spacing">
+        <p className="news-paragraph">
+          8. Deeper well drilling an unsustainable stopgap to groundwater
+          depletion
+          <a href="#" className="hacker-links">
+            (nature.com)
+          </a>
+          <p className="news-para">
+            77 points by
+            <a href="#" className="hacker-links">
+              jseliger
+            </a>
+            <a href="#" className="hacker-links">
+              4 hours ago
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              hide
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              32 comments
+            </a>
+          </p>
+        </p>
+      </div>
+      <div className="spacing">
+        <p className="news-paragraph">
+          9. The Internet's Old Guard
+          <a href="#" className="hacker-links">
+            (reading-supply)
+          </a>
+          <p className="news-para">
+            97 points by
+            <a href="#" className="hacker-links">
+              gordon
+            </a>
+            <a href="#" className="hacker-links">
+              6 hours ago
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              hide
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              92 comments
+            </a>
+          </p>
+        </p>
+      </div>
+      <div className="spacing">
+        <p className="news-paragraph">
+          10. Gubernator: Cloud-Native Distributed Rate Limiting for
+          Microservices
+          <a href="#" className="hacker-links">
+            (mailgun.com)
+          </a>
+          <p className="news-para">
+            87 points by
+            <a href="#" className="hacker-links">
+              rspivak
+            </a>
+            <a href="#" className="hacker-links">
+              4 hours ago
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              hide
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              52 comments
+            </a>
+          </p>
+        </p>
+      </div>
+      <div className="spacing">
+        <p className="news-paragraph">
+          11. Asana Outage
+          <a href="#" className="hacker-links">
+            (asana.com)
+          </a>
+          <p className="news-para">
+            187 points by
+            <a href="#" className="hacker-links">
+              bacondude
+            </a>
+            <a href="#" className="hacker-links">
+              1 hour ago
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              hide
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              82 comments
+            </a>
+          </p>
+        </p>
+      </div>
+      <div className="spacing">
+        <p className="news-paragraph">
+          12. The Internet's Old Guard
+          <a href="#" className="hacker-links">
+            (reading.supply)
+          </a>
+          <p className="news-para">
+            18 points by
+            <a href="#" className="hacker-links">
+              gordon
+            </a>
+            <a href="#" className="hacker-links">
+              3 hours ago
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              hide
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              70 comments
+            </a>
+          </p>
+        </p>
+      </div>
+      <div className="spacing">
+        <p className="news-paragraph">
+          13. Browser extensions are underrated: the promise of hackable
+          software
+          <a href="#" className="hacker-links">
+            (bloomberg.com)
+          </a>
+          <p className="news-para">
+            18 points by
+            <a href="#" className="hacker-links">
+              aniham
+            </a>
+            <a href="#" className="hacker-links">
+              9 hours ago
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              hide
+            </a>
+            |
+            <a href="#" className="hacker-links">
+              50 comments
+            </a>
+          </p>
+        </p>
+      </div>
+      <div>
+        <a href="#" className="more">
+          More
+        </a>
+        <hr className="un" />
+      </div>
     </div>
   );
 }
