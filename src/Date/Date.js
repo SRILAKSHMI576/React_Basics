23b973ca3216ca13f6de5662897e0a9b9a77c3cf
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-08-03 18:01:30 +0530

    Print Data according to given condition

--- src/Date/Date.js
+++ src/Date/Date.js
@@ -1,11 +1,25 @@
 import React from "react";
 
 function PresentTime() {
+  // PRINT WITH NAMES;
   //   const firstName = "Srilakshmi";
   //   const secondName = "srinivas";
   //   return <div>Hello {firstName + " " + secondName}</div>;
+  //    KNOW PRESENT DATE;
+  //   const date = new Date();
+  //   return <div> Time {date.getHours() % 12} o' clock!</div>;
   const date = new Date();
-  return <div> Time {date.getHours() % 12} o' clock!</div>;
+  const hours = date.getHours();
+  let timeOfDay;
+
+  if (hours < 12) {
+    timeOfDay = "morning";
+  } else if (hours >= 12) {
+    timeOfDay = "afternoon";
+  } else {
+    timeOfDay = "night";
+  }
+  return <hi>Good {timeOfDay}</hi>;
 }
 
 export default PresentTime;
